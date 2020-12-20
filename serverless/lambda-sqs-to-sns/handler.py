b7c3278484644cf4348d263092ebdb4066f038c5
gabmuniz1995 <gabrielmunizlima@gmail.com>
2020-12-19 22:41:07 -0300

    Update handler.py

--- serverless/lambda-sqs-to-sns/handler.py
+++ serverless/lambda-sqs-to-sns/handler.py
@@ -1,11 +1,13 @@
 from sqsHandler import SqsHandler
 from env import Variables
 import json
+import boto3
 
 
 def handler(event, context):
     env = Variables()
     sqs = SqsHandler(env.get_sqs_consulta_url())
+    sns = boto3.client('sns')
     
     #Do array de mensagens do tópico, recebe uma mensagem
     mensagens = sqs.getMessage(1)
@@ -14,5 +16,12 @@ def handler(event, context):
         for msg in mensagens['Messages']:
             #Aqui deve enviar para o SNS e depois excluir
             sqs.deleteMessage(msg['ReceiptHandle'])
+            # Publish a simple message to the specified SNS topic
+            response = sns.publish(
+            TopicArn=env.get_sns_dest_url(),    
+            Message=msg['Body'],    
+            )
+            # Print out the response
+            print(response)
     else:
         print("nao existem itens para enviar para o SNS")
